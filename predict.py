9b6f6fc1af2191e9802087c4a07d44807ce650a8
Alex <alexgeo2395@gmail.com>
2020-07-15 10:22:05 +0300

    Add documentation

--- predict.py
+++ predict.py
@@ -2,9 +2,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+# Specify CLI args and parse them
 parser.add_argument("image_path")
 parser.add_argument("model_path")
 args = parser.parse_args()
 
+# Get arg values
 image_path = args.image_path
 model_path = args.model_path
