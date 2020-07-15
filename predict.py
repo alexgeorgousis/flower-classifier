3e6fa1fa2d571173656d84005c2f55eaa1b70a63
Alex <alexgeo2395@gmail.com>
2020-07-15 10:20:47 +0300

    Get path to saved keras model from command line

--- predict.py
+++ predict.py
@@ -1,7 +1,10 @@
 import argparse
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument("image_path")
+parser.add_argument("model_path")
 args = parser.parse_args()
 
 image_path = args.image_path
+model_path = args.model_path
